4988737a561141a17311777f5b687c556e2e3ff2
pherget <pherget@gmail.com>
2016-04-18 19:02:34 -0700

    Added power reading and logging.

--- code/Python/CollectData_Rev_B.py
+++ code/Python/CollectData_Rev_B.py
@@ -11,6 +11,7 @@ PRIVATE_SERVICE = 'ACD63933AE9C393DA313F75B3E5F9A8E'
 PRIVATE_BAT_SERVICE = 'ACD63933AE9C393DA313F75B3E5F9A8B'
 PRIVATE_TEMP_SERVICE = 'ACD63933AE9C393DA313F75B3E5F9A8D'
 PRIVATE_MODE_SERVICE = 'ACD63933AE9C393DA313F75B3E5F9A8F'
+PRIVATE_POWER_SERVICE = 'ACD63933AE9C393DA313F75B3E5F9A8C'
 
 global fsm 
 fsm = None
@@ -161,6 +162,12 @@ def on_read_data(e):
 	foundData = 0
 	foundBattery = 0
 	foundTemp = 0
+	foundPower = 0
+
+	Vbat = 0
+	Temperature = 0
+	txPower = 0
+ 
 	temp = None
 	while temp != '' and btle.connected == 'yes':
 		temp = btle.read().strip()
@@ -181,6 +188,10 @@ def on_read_data(e):
 				temperature = splitList[2].strip('.')
 				if len(temperature) > 2:
 					foundTemp = 1
+			if splitList[1] == '0019':   # Check if it's the TX Power
+				txPower = splitList[2].strip('.')
+				if len(txPower) > 1:
+					foundPower = 1
 
 	# If we time out and are still connected	
 	if btle.connected == 'yes':
@@ -214,7 +225,6 @@ def on_read_data(e):
 				cap[i] = 0
 		print "S0: %.1fpF  S1: %.1fpF  S2: %.1fpF  S3: %.1fpF  S4: %.1fpF  S5: %.1fpF  S6: %.1fpF  " % (cap[0], cap[1], cap[2], cap[3], cap[4], cap[5], cap[6])
 	
-		Vbat = 0
 		if foundBattery:
 			# Convert the battery voltage
 			readBat = int(battery, 16)
@@ -224,7 +234,6 @@ def on_read_data(e):
 				Vbat = 0;
 			print "Battery Voltage: %1.2fV" % Vbat
 
-		Temperature = 0
 		if foundTemp:
 			readTemp = int(temperature, 16)
 			if readTemp > 0:
@@ -233,36 +242,16 @@ def on_read_data(e):
 				Temperature = 0;
 			print "Temperature: %3.1f Degrees F" % Temperature
 
-		sensorTimeout = 0 
-
-		# Get the timeout
-		#btle.write('CURV,' + PRIVATE_MODE_SERVICE, 'Read Timeout')
-		#gotTimeoutData = 0
-		#sensorTimeout = 0
-		#temp = None
-		#while temp != '' and gotTimeoutData == 0 and btle.connected == 'yes':
-		#	temp = btle.read()
-		#	splitList = temp.strip().strip('.').split(',')
-		#	if len(splitList) == 2 and splitList[0] == 'R' and len(splitList[1]) >= 1:
-		#		gotTimeoutData = 1
-		#		sensorTimeout = int(splitList[1], 16) * 5
-		#		print "Sensor Timeout : %d sec" % sensorTimeout
-
-		# Read the timeout setpoint from the text file
-		#fTimeout = open('timeout.txt', 'r')
-		#line = fTimeout.readline()
-		#timeout_setpoint = int(line.strip())
-		#fTimeout.close()
-
-		# Set the timeout if we were able to read it and it's not at the setpoint 
-		#if sensorTimeout > 0 and sensorTimeout != timeout_setpoint * 5:
-		#	btle.write('CUWV,' + PRIVATE_MODE_SERVICE + ',%02X' % timeout_setpoint, 'Write Timeout')
-		#	temp = None
-		#	while temp != '' and btle.connected == 'yes':
-		#		temp = btle.read()
+		if foundPower:
+			readPower = int(txPower, 16)
+			if readPower > 0:
+				txPower = readPower
+			else:
+				txPower = 0;
+			print "txPower = %d" % txPower
 
 		dataFile = open(DataPath + BTA + '.txt', 'a') 
-		print >> dataFile, "%f, %4d, %4d, %4d, %4d, %4d, %4d, %4d, %1.2f, %3.1f, %d" % (time.time(), sensor[0], sensor[1], sensor[2], sensor[3], sensor[4], sensor[5], sensor[6], Vbat, Temperature, sensorTimeout)
+		print >> dataFile, "%f, %4d, %4d, %4d, %4d, %4d, %4d, %4d, %1.2f, %3.1f, %d" % (time.time(), sensor[0], sensor[1], sensor[2], sensor[3], sensor[4], sensor[5], sensor[6], Vbat, Temperature, txPower)
 		dataFile.flush()
 		dataFile.close()
 
